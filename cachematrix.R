d2050f54d10a68e15b77f01e05520e49806bf384
Xinran603 <xinranliu603@gmail.com>
2015-11-22 14:31:49 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My function is to look at whether an input matrix is cached or not, if yes, return directly the resulf; if not, calculate and return the result
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function to create the inverse of the input matrix, cacheSolve is a function to check if the input matrix is cached or not.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversematrix <- NULL
+  set <- function(y){
+    x <<- y
+    inversematrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) matrixinverse <<- solve
+  getinverse <- function() matrixinverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixinverse <- x$getinverse()
+  if(!is.null(matrixinverse)){
+    message("getting cached data")
+    return(matrixinverse)
+  }
+  data <- x$get()
+  matrixinverse <- solve(data, ...)
+  x$setinverse(matrixinverse)
+  matrixinverse
 }
+
